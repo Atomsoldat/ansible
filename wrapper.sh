f2364342a2fe19ddbaa30c0130bb6ac1864af6e0
Leon Welchert <lichturm@posteo.de>
2024-08-04 12:50:53 +0200

    add handy wrapper script for playbooks

--- wrapper.sh
+++ wrapper.sh
@@ -6,5 +6,8 @@ case $arg in
 	software)
 		ansible-playbook  playbook_computer_setup.yaml --ask-become-pass -e 'host_type=desktop'
 	;;
+	shares)
+		ansible-playbook  playbook_nas_client.yaml --ask-become-pass -e 'host_type=desktop'
+	;;
 esac
 
