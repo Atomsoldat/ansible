5b889205d6cccfbad8a04b3a5fc56ee41e49f41a
Leon Welchert <lichturm@posteo.de>
2025-07-14 22:47:21 +0200

    move playbooks to separate directory

--- wrapper.sh
+++ wrapper.sh
@@ -4,23 +4,23 @@ arg=$1
 
 case $arg in
 	repos)
-		ansible-playbook  playbook_repo_setup.yaml --ask-become-pass -e 'host_type=workstation'
+		ansible-playbook  playbooks/playbook_repo_setup.yaml --ask-become-pass -e 'host_type=workstation'
 	;;
 	software)
-		ansible-playbook  playbook_software_installation.yaml --ask-become-pass -e 'host_type=workstation'
+		ansible-playbook  playbooks/playbook_software_installation.yaml --ask-become-pass -e 'host_type=workstation'
 	;;
 	config)
-		ansible-playbook  playbook_config.yaml --ask-become-pass -e 'host_type=workstation'
+		ansible-playbook  playbooks/playbook_config.yaml --ask-become-pass -e 'host_type=workstation'
 	;;
 	shares)
-		ansible-playbook  playbook_nas_client.yaml --ask-become-pass -e 'host_type=workstation'
+		ansible-playbook  playbooks/playbook_nas_client.yaml --ask-become-pass -e 'host_type=workstation'
 	;;
 	fonts)
-		ansible-playbook  playbook_fonts.yaml --ask-become-pass -e 'host_type=workstation'
+		ansible-playbook  playbooks/playbook_fonts.yaml --ask-become-pass -e 'host_type=workstation'
 	;;
 	home)
 		echo "INFO: Note, that the homedir_setup role needs a valid SSH key in the ssh-agent"
-		ansible-playbook  playbook_homedir_setup.yaml --ask-become-pass -e 'host_type=workstation'
+		ansible-playbook  playbooks/playbook_homedir_setup.yaml --ask-become-pass -e 'host_type=workstation'
 	;;
 esac
 
