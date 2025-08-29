91a48c0e15f5d793d6b71e8dcfd662452346e33f
Leon Welchert <lichturm@posteo.de>
2025-08-29 18:34:52 +0200

    remove playbook filename prefix

--- wrapper.sh
+++ wrapper.sh
@@ -4,26 +4,26 @@ arg=$1
 
 case $arg in
 	repos)
-		ansible-playbook  playbooks/playbook_repo_setup.yaml --ask-become-pass  -i inventory.yaml
+		ansible-playbook  playbooks/repo_setup.yaml --ask-become-pass  -i inventory.yaml
 	;;
 	software)
-		ansible-playbook  playbooks/playbook_software_installation.yaml --ask-become-pass  -i inventory.yaml
+		ansible-playbook  playbooks/software_installation.yaml --ask-become-pass  -i inventory.yaml
 	;;
 	scripts)
-		ansible-playbook  ./playbooks/playbook_script_installation.yaml --ask-become-pass -i inventory.yaml
+		ansible-playbook  playbooks/script_installation.yaml --ask-become-pass -i inventory.yaml
 	;;
 	config)
-		ansible-playbook  playbooks/playbook_config.yaml --ask-become-pass -i inventory.yaml
+		ansible-playbook  playbooks/config.yaml --ask-become-pass -i inventory.yaml
 	;;
 	shares)
-		ansible-playbook  playbooks/playbook_nas_client.yaml --ask-become-pass -i inventory.yaml
+		ansible-playbook  playbooks/nas_client.yaml --ask-become-pass -i inventory.yaml
 	;;
 	fonts)
-		ansible-playbook  playbooks/playbook_fonts.yaml --ask-become-pass -i inventory.yaml
+		ansible-playbook  playbooks/fonts.yaml --ask-become-pass -i inventory.yaml
 	;;
 	home)
 		echo "INFO: Note, that the homedir_setup role needs a valid SSH key in the ssh-agent"
-		ansible-playbook  playbooks/playbook_homedir_setup.yaml --ask-become-pass -i inventory.yaml
+		ansible-playbook  playbooks/homedir_setup.yaml --ask-become-pass -i inventory.yaml
 	;;
 esac
 
