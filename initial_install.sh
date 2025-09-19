dee2508f00609c48c65d47a86e9e718f0fcac120
Leon Welchert <lichturm@posteo.de>
2025-09-19 20:57:02 +0200

    store sudo password in variable during setup script

--- initial_install.sh
+++ initial_install.sh
@@ -8,22 +8,25 @@
 
 set -euo pipefail
 
+echo "Enter sudo password to be used by ansible for privileged tasks"
+read password
 
 # set up debian testing repos
 # as well as all other repos we need
-./wrapper.sh repos
+ansible-playbook  playbooks/repo_setup.yaml --become-password-file <(printf "${password}")  -i inventory.yaml
 
 sudo apt-get update && sudo apt-get dist-upgrade
 sudo apt-get autoremove
 
 # install all desired software
-./wrapper.sh software
+ansible-playbook  playbooks/software_installation.yaml --become-password-file <(printf "${password}")  -i inventory.yaml
 
-./wrapper.sh scripts
+ansible-playbook  playbooks/script_installation.yaml --become-password-file <(printf "${password}") -i inventory.yaml
 
-./wrapper.sh config
+ansible-playbook  playbooks/config.yaml --become-password-file <(printf "${password}") -i inventory.yaml
 
-./wrapper.sh fonts
-./wrapper.sh storage
-./wrapper.sh home
+ansible-playbook  playbooks/fonts.yaml --become-password-file <(printf "${password}") -i inventory.yaml
+ansible-playbook  playbooks/storage.yaml --become-password-file <(printf "${password}") -i inventory.yaml
 
+echo "INFO: Note, that the homedir_setup role needs a valid SSH key in the ssh-agent"
+ansible-playbook  playbooks/homedir_setup.yaml --become-password-file <(printf "${password}") -i inventory.yaml
