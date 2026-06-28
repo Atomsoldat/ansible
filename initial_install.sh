049f88f6eead61b7e50000efdb545d323434fa68
Leon Welchert <lichturm@posteo.de>
2026-06-28 20:47:20 +0200

    refactor: simplify initial_install.sh by asking for sudo password again

--- initial_install.sh
+++ initial_install.sh
@@ -1,38 +1,13 @@
 #!/bin/bash
 
-###
-### This script should be run as a regular user, all tasks requiring root permissions will
-### use "become: true". Some tasks e.g. during the installation of homebrew
-### should be run as a regular user, they will fail when run as root
-###
+# This script should be run as a regular user,
+# all tasks requiring root permissions will use "become: true"
 
 set -euo pipefail
 
-ansible_password_file="${HOME}/.ansible_becomepass_temp"
-
-touch ${ansible_password_file}
-chmod go-rwx ${ansible_password_file}
-
-function cleanup {
-    rm ${ansible_password_file}
-}
-
-trap cleanup EXIT
-
-
-echo "Enter sudo password to be used by ansible for privileged tasks"
-# set empty IFS so read will read until the newline
-IFS= read -sr password
-
-echo ${password} > $ansible_password_file
-
-eval $(ssh-agent)
-ssh-add
-
-
 # set up debian testing repos
 # as well as all other repos we need
-ansible-playbook  playbooks/repo_setup.yaml --become-password-file ${ansible_password_file}  -i inventory.yaml
+ansible-playbook  playbooks/repo_setup.yaml --ask-become-pass -i inventory.yaml
 
 source /etc/os-release
 
@@ -43,15 +18,15 @@ fi
 
 
 # install all desired software
-ansible-playbook  playbooks/software_installation.yaml --become-password-file ${ansible_password_file}  -i inventory.yaml
+ansible-playbook  playbooks/software_installation.yaml --ask-become-pass -i inventory.yaml
 
-ansible-playbook  playbooks/script_installation.yaml --become-password-file ${ansible_password_file} -i inventory.yaml
-ansible-playbook  playbooks/config.yaml --become-password-file ${ansible_password_file} -i inventory.yaml
+ansible-playbook  playbooks/script_installation.yaml --ask-become-pass -i inventory.yaml
+ansible-playbook  playbooks/config.yaml --ask-become-pass -i inventory.yaml
 
 
-ansible-playbook  playbooks/fonts.yaml --become-password-file ${ansible_password_file} -i inventory.yaml
-ansible-playbook  playbooks/storage.yaml --become-password-file ${ansible_password_file} -i inventory.yaml
+ansible-playbook  playbooks/fonts.yaml --ask-become-pass -i inventory.yaml
+ansible-playbook  playbooks/storage.yaml --ask-become-pass -i inventory.yaml
 
 echo "INFO: Note, that the homedir_setup role needs a valid SSH key in the ssh-agent"
-ansible-playbook  playbooks/homedir_setup.yaml --become-password-file ${ansible_password_file} -i inventory.yaml
+ansible-playbook  playbooks/homedir_setup.yaml --ask-become-pass -i inventory.yaml
 
