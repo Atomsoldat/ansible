e3b39257f44b95a8ed6c9f7c37e247533abfd160
Leon Welchert <lichturm@posteo.de>
2026-06-29 08:45:13 +0200

    refactor: do not set up storage during initial_install.sh

--- initial_install.sh
+++ initial_install.sh
@@ -5,6 +5,9 @@
 
 set -euo pipefail
 
+ansible-playbook  playbooks/essential_packages.yaml --ask-become-pass -i inventory.yaml
+ansible-playbook  playbooks/repo_setup.yaml --ask-become-pass -i inventory.yaml
+
 # set up debian testing repos
 # as well as all other repos we need
 ansible-playbook  playbooks/repo_setup.yaml --ask-become-pass -i inventory.yaml
@@ -16,6 +19,7 @@ if [[ "$PRETTY_NAME" == "Debian" ]]; then
   sudo apt-get autoremove
 fi
 
+ansible-playbook  playbooks/homedir_setup.yaml --ask-become-pass -i inventory.yaml
 
 # install all desired software
 ansible-playbook  playbooks/software_installation.yaml --ask-become-pass -i inventory.yaml
@@ -25,8 +29,3 @@ ansible-playbook  playbooks/config.yaml --ask-become-pass -i inventory.yaml
 
 
 ansible-playbook  playbooks/fonts.yaml --ask-become-pass -i inventory.yaml
-ansible-playbook  playbooks/storage.yaml --ask-become-pass -i inventory.yaml
-
-echo "INFO: Note, that the homedir_setup role needs a valid SSH key in the ssh-agent"
-ansible-playbook  playbooks/homedir_setup.yaml --ask-become-pass -i inventory.yaml
-
